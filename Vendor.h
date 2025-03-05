7ba622a189cce2c6e9e8ff96acb23c94e49aa1ef
Tyler <tylerduong9@gmail.com>
2025-03-04 22:27:27 -0800

    Starting Vendor.h

--- Vendor.h
+++ Vendor.h
@@ -1,24 +1,11 @@
-#ifndef VENDOR_H
-#define VENDOR_H
-
 // TO DO: #include all the standard libraries and your own libraries here
+#ifndef VENDER.H
+#define VENDER.H
+#include <iostream>
 #include <string>
 
 // To DO: define the class Vendor with the necessary functions and data fields
-
 class Vendor {
-public:
-    Vendor();
-    ~Vendor(); //destructor
-    // This is a function that allows you to compare two vendors based on their username and email address.  
-    // You may directly include it in your class definition. 
-    // You don't need to modify it but will have to put it inside your class. 
-    // Operator == overloading function prototype:
-    bool operator==(const Vendor& otherVendor) const; 
-    void displayProfile();
-    bool modifyPassword(std::string newPassword);
-
-
     private:
         std::string username;
         std::string email;
@@ -26,6 +13,27 @@ public:
         std::string bio;
         std::string profilePicture;
 
-};
+    public:
+        void displayProfile();
+        bool modifyPassword(std::string newPassword);
+        bool createProduct(product Product);
+        void displayProduct(int k);
+        bool modifyProduct(int k);
+        bool sellProduct(int k,int quantity);
+        bool deleteProduct(int k);
+        bool operator==(const Vendor& otherVendor) const; 
+
+
+
+
+
+
+
+    };
+// This is a function that allows you to compare two vendors based on their username and email address.
+// You may directly include it in your class definition. 
+// You don't need to modify it but will have to put it inside your class. 
+// Operator == overloading function prototype:
+
 
 #endif
